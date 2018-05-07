bc936d9e43dce466196baee65a42cb406d01d5fd
bandiare2 <bandiare2@gmail.com>
2018-05-07 10:06:56 +0000

    my first commit

---
+++ FormulaireVueJs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulaire </title>
+	<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="app.js"></script>
+
+<body>
+
+	<div>
+		
+	</div>
+
+	<div></div>
+	<div id="rouge"></div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</body>
+</html>
